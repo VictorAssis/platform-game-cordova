c23b408a6efcd6e87fb463884f8cc11599dba62f
VictorAssis <contato@victorassis.com.br>
2019-12-15 19:49:10 -0300

    add controls in screen for mobile devices

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -3,6 +3,7 @@ import dudeImg from '../assets/dude.png'
 import platformImg from '../assets/platform.png'
 import skyImg from '../assets/sky.png'
 import starImg from '../assets/star.png'
+import controlsImg from '../assets/controls.png'
 import Phaser from 'Phaser'
 
 class Play extends Phaser.Scene {
@@ -11,6 +12,11 @@ class Play extends Phaser.Scene {
 
     this.score = 0
     this.gameOver = false
+    this.holding = {
+      left: false,
+      right: false,
+      jump: false
+    }
   }
 
   preload () {
@@ -21,10 +27,17 @@ class Play extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 48
     })
+    this.load.spritesheet('controls', controlsImg, {
+      frameWidth: 50,
+      frameHeight: 50
+    })
     this.load.image('star', starImg)
   }
 
   create () {
+    // Add more than one touch events at same time
+    this.input.addPointer(1)
+
     // Render the background
     this.add.image(400, 300, 'sky')
 
@@ -32,6 +45,7 @@ class Play extends Phaser.Scene {
     this.createPlayer()
     this.createStars()
     this.createBomb()
+    this.createUi()
     this.setColiders()
 
     this.cursors = this.input.keyboard.createCursorKeys()
@@ -41,27 +55,27 @@ class Play extends Phaser.Scene {
 
   update () {
     if (this.gameOver) return
-    if (this.cursors.left.isDown) {
+    if (this.cursors.left.isDown || this.holding.left) {
       this.player.setVelocityX(-160)
       this.player.anims.play('left', true)
-    } else if (this.cursors.right.isDown) {
+    } else if (this.cursors.right.isDown || this.holding.right) {
       this.player.setVelocityX(160)
       this.player.anims.play('right', true)
     } else {
       this.player.setVelocityX(0)
       this.player.anims.play('turn')
     }
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
+    if ((this.cursors.up.isDown || this.holding.jump) && this.player.body.touching.down) {
       this.player.setVelocityY(-330)
     }
   }
 
   createPlatforms () {
     this.platforms = this.physics.add.staticGroup()
-    this.platforms.create(320, 353, 'ground').setScale(1.6).refreshBody()
-    this.platforms.create(575, 250, 'ground')
-    this.platforms.create(50, 175, 'ground')
-    this.platforms.create(690, 120, 'ground')
+    this.platforms.create(320, 330, 'ground').setScale(1.875).refreshBody()
+    this.platforms.create(530, 210, 'ground').setScale(0.8).refreshBody()
+    this.platforms.create(50, 150, 'ground').setScale(0.8).refreshBody()
+    this.platforms.create(640, 100, 'ground').setScale(0.8).refreshBody()
   }
 
   createPlayer () {
@@ -103,6 +117,26 @@ class Play extends Phaser.Scene {
     this.bombs = this.physics.add.group()
   }
 
+  createUi () {
+    this.btnLeft = this.add.sprite(35, 330, 'controls')
+    this.btnLeft.setInteractive()
+    this.btnLeft.on('pointerdown', () => { this.holding.left = true }, this)
+    this.btnLeft.on('pointerup', () => { this.holding.left = false }, this)
+    this.btnLeft.setAlpha(0.5)
+
+    this.btnRight = this.add.sprite(95, 330, 'controls', 1)
+    this.btnRight.setInteractive()
+    this.btnRight.on('pointerdown', () => { this.holding.right = true }, this)
+    this.btnRight.on('pointerup', () => { this.holding.right = false }, this)
+    this.btnRight.setAlpha(0.5)
+
+    this.btnJump = this.add.sprite(605, 330, 'controls', 2)
+    this.btnJump.setInteractive()
+    this.btnJump.on('pointerdown', () => { this.holding.jump = true }, this)
+    this.btnJump.on('pointerup', () => { this.holding.jump = false }, this)
+    this.btnJump.setAlpha(0.5)
+  }
+
   setColiders () {
     this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this)
     this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this)
@@ -132,8 +166,8 @@ class Play extends Phaser.Scene {
 
   hitBomb (player, bomb) {
     this.physics.pause()
-    this.player.setTint(0xff0000)
-    this.player.anims.play('turn')
+    player.setTint(0xff0000)
+    player.anims.play('turn')
     this.gameOver = true
   }
 }
